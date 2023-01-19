9a946a647ab2266ac5afa8b0815dfc18036a10da
Ibnballo1 <belloabdullateef035@gmail.com>
2023-01-19 18:38:05 +0100

    Update animal tables inside a transaction

--- queries.sql
+++ queries.sql
@@ -10,3 +10,21 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Day 2
+BEGIN WORK;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animalsSET species = 'pokemon'
+WHERE name NOT LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
+
