7451e72b6e35110dc953f51c540f2c6d5b0fa62e
Ibnballo1 <belloabdullateef035@gmail.com>
2023-01-20 08:44:44 +0100

    Create table owner

--- schema.sql
+++ schema.sql
@@ -14,3 +14,10 @@ CREATE TABLE animals (
 -- Day 2
 ALTER TABLE animals
 ADD species VARCHAR(100);
+
+-- Day 3
+CREATE TABLE OWNERS (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  full_name VARCHAR(250) NOT NULL,
+  age INT NOT NULL
+);
